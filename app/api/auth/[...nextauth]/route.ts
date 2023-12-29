79456a12e616a2129381e5ef3da9196bdc098888
Shiva953 <b22070@students.iitmandi.ac.in>
2023-12-29 09:20:50 +0530

    some changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,20 @@
-import NextAuth from "next-auth/next"
-import { authOptions } from "@/app/utils/authOptions";
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
 
-export const handler = NextAuth(authOptions); //adding github oauth in next auth options
+const authOptions = { //defining the auth providers(in this case, github)
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    })
+  ],
+};
+
+const handler = NextAuth(authOptions); //adding github oauth in next auth options
 
 export { handler as GET, handler as POST };
